80cfe08b76406eaec70a02b4a5641e7486fe7305
Jalal Zairi <jzairi@smb.sqli.com>
2022-04-16 17:20:53 +0000

    resolve images import problem

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
+  carousel1:string = "assets/images/carousel1.jpg";
+  carousel2:string = "assets/images/carousel2.jpg";
+  carousel3:string = "assets/images/carousel3.jpg";
 
   constructor() { }
 
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  logo:string = "assets/images/logo.png";
   constructor() { }
 
   ngOnInit(): void {
